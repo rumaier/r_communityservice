f5459dd73344a5371e89f21e03aeea388e939a25
r_ <jayyc818@gmail.com>
2024-10-25 12:28:37 -0500

    refactor: removed leftover config value

--- config.lua
+++ config.lua
@@ -12,7 +12,6 @@ Cfg = {
     -- |___/\___|_|    \_/ \___|_|
     Server = {
         language = 'en',          -- Determines the language. ('en': English, 'es': Spanish, 'fr': French, 'de': German, 'pt': Portuguese, 'zh': Chinese)
-        notification = 'default', -- Determines the notification system. ('default', 'ox', 'custom': can be customized in bridge/framework/YOURFRAMEWORK)
         versionCheck = true,      -- Determines if the version check is enabled.
         policeJobs = {            -- Determines the police jobs, you can add as many as you need.
             'police',
