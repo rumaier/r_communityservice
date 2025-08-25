be4f57f5df1d62e94c9b88583c2aa8361134071b
rumaier <148654629+rumaier@users.noreply.github.com>
2025-08-25 15:28:35 -0500

    chore: setup base config

--- configs/cfg_main.lua
+++ configs/cfg_main.lua
@@ -1,9 +1,10 @@
---    _           _ _                 _       _
---   | |__   ___ (_) | ___ _ __ _ __ | | __ _| |_ ___
---   | '_ \ / _ \| | |/ _ \ '__| '_ \| |/ _` | __/ _ \
---   | |_) | (_) | | |  __/ |  | |_) | | (_| | ||  __/
---   |_.__/ \___/|_|_|\___|_|  | .__/|_|\__,_|\__\___|
---                             |_|
+--                                                _ _                             _
+-- _ __  ___ ___  _ __ ___  _ __ ___  _   _ _ __ (_) |_ _   _ ___  ___ _ ____   _(_) ___ ___
+-- | '__|/ __/ _ \| '_ ` _ \| '_ ` _ \| | | | '_ \| | __| | | / __|/ _ \ '__\ \ / / |/ __/ _ \
+-- | |  | (_| (_) | | | | | | | | | | | |_| | | | | | |_| |_| \__ \  __/ |   \ V /| | (_|  __/
+-- |_|___\___\___/|_| |_| |_|_| |_| |_|\__,_|_| |_|_|\__|\__, |___/\___|_|    \_/ |_|\___\___|
+--  |_____|                                              |___/
+--
 --  Need support? Join our Discord server for help: https://discord.gg/rscripts
 --
 Cfg = {
@@ -22,39 +23,18 @@ Cfg = {
     --  \___/| .__/ \__|_|\___/|_| |_|___/
     --       |_|
     Options = {
-        NuiColor = 'violet' -- Colors: ('dark', 'gray', 'red', 'pink', 'grape', 'violet', 'indigo', 'blue', 'cyan', 'teal', 'green', 'lime', 'yellow', 'orange')
-    },
-    --              _  __
-    --  _   _ _ __ (_)/ _| ___  _ __ _ __ ___
-    -- | | | | '_ \| | |_ / _ \| '__| '_ ` _ \
-    -- | |_| | | | | |  _| (_) | |  | | | | | |
-    --  \__,_|_| |_|_|_|  \___/|_|  |_| |_| |_|
-    Uniform = {
-        Enabled = true, -- Wardrobe System (true: enabled, false: disabled)
-        Outfit = {
-            Male = {
-                ['arms'] = 0,
-                ['tshirt_1'] = 15, ['tshirt_2'] = 0,
-                ['torso_1'] = 86, ['torso_2'] = 0,
-                ['bproof_1'] = 0, ['bproof_2'] = 0,
-                ['decals_1'] = 0, ['decals_2'] = 0,
-                ['chain_1'] = 0, ['chain_2'] = 0,
-                ['pants_1'] = 10, ['pants_2'] = 2,
-                ['shoes_1'] = 56, ['shoes_2'] = 0,
-                ['helmet_1'] = 14, ['helmet_2'] = 0,
-            },
-            Female = {
-                ['arms'] = 0,
-                ['tshirt_1'] = 15, ['tshirt_2'] = 0,
-                ['torso_1'] = 86, ['torso_2'] = 0,
-                ['bproof_1'] = 0, ['bproof_2'] = 0,
-                ['decals_1'] = 0, ['decals_2'] = 0,
-                ['chain_1'] = 0, ['chain_2'] = 0,
-                ['pants_1'] = 10, ['pants_2'] = 2,
-                ['shoes_1'] = 56, ['shoes_2'] = 0,
-                ['helmet_1'] = 14, ['helmet_2'] = 0,
-            },
+        NuiColor = 'violet', -- Colors: ('dark', 'gray', 'red', 'pink', 'grape', 'violet', 'indigo', 'blue', 'cyan', 'teal', 'green', 'lime', 'yellow', 'orange')
+
+        PoliceJobs = { -- Jobs that can assign tasks
+            'police',
+            -- 'sheriff',
         },
+
+        ZoneCoords = vec3(1493.966, 2405.158, 48.644), -- Zone coordinates (vector3)
+        ZoneRadius = 55.0,                             -- Zone radius (number)
+
+        MaxTasks = 60, -- Maximum number of tasks admins/police can assign (number)
+        TaskTime = 10, -- Time (in seconds) to complete each task (number)
     },
     --      _      _
     --   __| | ___| |__  _   _  __ _
