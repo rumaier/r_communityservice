75b3fecddc0aa82b33e4fb0de881760934a07950
rumaier <148654629+rumaier@users.noreply.github.com>
2025-08-28 21:12:46 -0500

    feat: enhance community service logic with inventory management and error handling

--- core/server/main.lua
+++ core/server/main.lua
@@ -2,6 +2,32 @@ CommsCache = {}
 local serving = {}
 local zone = lib.zones.sphere({ coords = Cfg.Options.ZoneCoords, radius = Cfg.Options.ZoneRadius })
 
+lib.callback.register('r_communityservice:runPlayerRelogCheck', function(src)
+    local identifier = Core.Framework.getPlayerIdentifier(src)
+    if not identifier then _debug('[^1ERROR^0] - Could not fetch identifier for player ID: ' .. src) return false end
+    local data = CommsCache[identifier]
+    if not data then _debug('[^1ERROR^0] - No community service record found for player ID: ' .. src) return false end
+    serving[src] = true
+    return true, data.tasks_remaining
+end)
+
+local function confiscateInventory(src)
+    local items = Core.Inventory.getPlayerInventory(src) or {}
+    for _, item in pairs(items) do
+        local removed = Core.Inventory.removeItem(src, item.name, item.count)
+        if not removed then _debug('[^1ERROR^0] - Failed to remove item: ' .. item.name .. ' from player: ' .. src) return false end
+    end
+    return items
+end
+
+local function returnInventory(src, items)
+    for _, item in pairs(items) do
+        local added = Core.Inventory.addItem(src, item.name, item.count, item.metadata)
+        if not added then _debug('[^1ERROR^0] - Failed to return item: ' .. item.name .. ' to player: ' .. src) return false end
+    end
+    return true
+end
+
 local function getPlayerAccess(src)
     local access = 0
     local ace = IsPlayerAceAllowed(src, 'communityservice')
@@ -17,8 +43,11 @@ end
 lib.callback.register('r_communityservice:getPlayerAccess', getPlayerAccess)
 
 local function releasePlayer(src, identifier)
+    local inventory = CommsCache[identifier] and CommsCache[identifier].stored_items or {}
+    local invReturned = returnInventory(src, inventory)
+    if not invReturned then _debug('[^1ERROR^0] - Failed to return inventory to player ID: ' .. src) return false, 'sv_err' end
     local deleted = MySQL.query.await('DELETE FROM r_communityservice WHERE identifier = ?', { identifier })
-    if not deleted then _debug('[^1ERROR^0] - Failed to delete database record for player ID: ' .. src) return false end
+    if not deleted then _debug('[^1ERROR^0] - Failed to delete database record for player ID: ' .. src) return false, 'sv_err' end
     CommsCache[identifier] = nil
     if Cfg.Options.WebhookEnabled then --[[ //TODO: implement webhook  ]] end
     _debug('[^6DEBUG^0] - Releasing player: ' .. src .. ' from community service.')
@@ -29,31 +58,31 @@ end
 
 lib.callback.register('r_communityservice:requestTask', function(src)
     local identifier = Core.Framework.getPlayerIdentifier(src)
-    if not identifier then _debug('[^1ERROR^0] - Could not fetch identifier for player ID: ' .. src) return false end
+    if not identifier then _debug('[^1ERROR^0] - Could not fetch identifier for player ID: ' .. src) return false, 'sv_err' end
     local data = CommsCache[identifier]
-    if not data then _debug('[^1ERROR^0] - No community service record found for player ID: ' .. src) return false end
+    if not data then _debug('[^1ERROR^0] - No community service record found for player ID: ' .. src) return false, 'sv_err' end
     if type(serving[src]) == 'vector3' then
         _debug('[^6DEBUG^0] - Distance checking player '.. src ..' at task location: ' .. tostring(serving[src]))
         local player = GetPlayerPed(src)
         local playerCoords = GetEntityCoords(player)
         local dist = #(playerCoords.xy - serving[src].xy)
-        if dist > 2.0 then _debug('[^1ERROR^0] - Player: ' .. src .. ' is too far from their task location. Distance: ' .. dist) return false end
+        if dist > 2.0 then _debug('[^1ERROR^0] - Player: ' .. src .. ' is too far from their task location. Distance: ' .. dist) return false, 'sv_err' end
     end
-    if data.tasks == 0 then return releasePlayer(src, identifier) end
-    data.tasks = data.tasks - 1
+    if data.tasks_remaining == 0 then return releasePlayer(src, identifier) end
+    data.tasks_remaining = data.tasks_remaining - 1
     local updated = MySQL.update.await([[
         UPDATE r_communityservice
         SET tasks_remaining = tasks_remaining - 1
         WHERE identifier = ? AND tasks_remaining > 0;
     ]], { identifier })
-    if updated == 0 then _debug('[^1ERROR^0] - Failed to update database for player ID: ' .. src) return false end
+    if updated == 0 then _debug('[^1ERROR^0] - Failed to update database for player ID: ' .. src) return false, 'sv_err' end
     local radius = (zone.radius or Cfg.Options.ZoneRadius) - 5.0
     repeat
         serving[src] = vec3(zone.coords.x + math.random(-radius, radius), zone.coords.y + math.random(-radius, radius), zone.coords.z)
         Wait(100)
     until zone:contains(serving[src])
     _debug('[^6DEBUG^0] - Sending coords: ' .. serving[src] .. ' to player: ' .. src)
-    return serving[src], data.tasks
+    return serving[src], data.tasks_remaining
 end)
 
 lib.callback.register('r_communityservice:assignCommunityService', function(src, target, tasks)
@@ -61,13 +90,15 @@ lib.callback.register('r_communityservice:assignCommunityService', function(src,
     if access < 2 then _debug('[^1ERROR^0] - Player '.. src ..' insufficient perms to assign comms') return false end
     local identifier = Core.Framework.getPlayerIdentifier(target)
     if not identifier then return false, _L('player_not_found', target) end
+    local inventory = confiscateInventory(target)
+    if not inventory then _debug('[^1ERROR^0] - Failed to confiscate inventory from player ID: ' .. target) return false end
     local inserted = MySQL.insert.await([[
-        INSERT INTO r_communityservice (identifier, tasks_remaining)
-        VALUES (?, ?)
-        ON DUPLICATE KEY UPDATE tasks_remaining = VALUES(tasks_remaining)
-    ]], { identifier, tasks })
+        INSERT INTO r_communityservice (identifier, tasks_remaining, stored_items)
+        VALUES (?, ?, ?)
+        ON DUPLICATE KEY UPDATE tasks_remaining = VALUES(tasks_remaining), stored_items = VALUES(stored_items)
+    ]], { identifier, tasks, json.encode(inventory) })
     if not inserted then _debug('[^1ERROR^0] - Failed to insert/update database for player ID: ' .. target) return false end
-    CommsCache[identifier] = { identifier = identifier, tasks = tasks }
+    CommsCache[identifier] = { identifier = identifier, tasks_remaining = tasks, stored_items = inventory }
     if Cfg.Options.WebhookEnabled then --[[ //TODO: implement webhook  ]] end
     _debug('[^6DEBUG^0] - Assigned ' .. tasks .. ' tasks to ID: ' .. target ..', sending player...')
     TriggerClientEvent('r_communityservice:teleportToCommunityService', target, tasks)
@@ -96,16 +127,21 @@ lib.callback.register('r_communityservice:fetchAssignedPlayers', function(src)
         local identifier = Core.Framework.getPlayerIdentifier(id)
         if not identifier then _debug('[^1ERROR^0] - Could not fetch identifier for player ID: ' .. id) return end
         local data = CommsCache[identifier]
-        if data then table.insert(active, { id = id, name = GetPlayerName(id), tasks = data.tasks }) end
+        if data then table.insert(active, { id = id, name = GetPlayerName(id), tasks_remaining = data.tasks_remaining }) end
     end
     _debug('[^6DEBUG^0] - Fetched ' .. #active .. ' active assigned players from cache.')
     return active
 end)
 
 local function cacheDatabase()
+    repeat Wait(0) until DatabaseBuilt
     local data = MySQL.query.await('SELECT * FROM r_communityservice')
     for _, row in pairs(data) do
-        CommsCache[row.identifier] = row
+        CommsCache[row.identifier] = {
+            identifier = row.identifier,
+            tasks_remaining = row.tasks_remaining,
+            stored_items = json.decode(row.stored_items)
+        }
     end
     _debug('[^6DEBUG^0] - Cached ' .. #data .. ' community service records from database.')
 end
@@ -113,12 +149,12 @@ end
 local function syncDatabase()
     local synced = 0
     for identifier, entry in pairs(CommsCache) do
-        local synced = MySQL.update.await([[
+        local updated = MySQL.update.await([[
             UPDATE r_communityservice
-            SET tasks_remaining = ?
+            SET tasks_remaining = ?, stored_items = ?
             WHERE identifier = ?;
-        ]], { entry.tasks, identifier })
-        if synced then
+        ]], { entry.tasks_remaining, json.encode(entry.stored_items), identifier })
+        if updated and updated > 0 then
             synced = synced + 1
         else
             _debug('[^1ERROR^0] - Failed to sync database for identifier: ' .. identifier)
@@ -143,4 +179,16 @@ AddEventHandler('onResourceStart', function(resource)
     if resource ~= GetCurrentResourceName() then return end
     registerInteractCommand()
     cacheDatabase()
+end)
+
+AddEventHandler('playerDropped', function()
+    local src = source
+    if serving[src] then
+        local identifier = Core.Framework.getPlayerIdentifier(src)
+        local data = CommsCache[identifier]
+        data.tasks_remaining = data.tasks_remaining + 1
+        serving[src] = nil
+        _debug('[^6DEBUG^0] - Player ' .. src .. ' dropped while serving, incremented current task.')
+        syncDatabase()
+    end
 end)
